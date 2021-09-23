8123a1930880cfbe1e4a6b24e3d43709454a564a
Shyamashree <shyamashree.mohanty@gmail.com>
2021-09-23 20:52:55 +0530

    added register API

--- pages/MyRegisterPage.js
+++ pages/MyRegisterPage.js
@@ -3,6 +3,7 @@ import React from 'react';
 import VisibilityIcon from '@material-ui/icons/Visibility';
 import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
 import dynamic from 'next/dynamic';
+import registerAPI from "./api/register";
 
 const TextField = dynamic(() => import('@material-ui/core/TextField'), { ssr: false });
 const RadioGroup = dynamic(() => import('@material-ui/core/RadioGroup'), { ssr: false });
@@ -12,15 +13,18 @@ export default function Register() {
     const [firstName, setFirstName] = React.useState('');
     const [lastName, setLastName] = React.useState('');
     const [email, setEmail] = React.useState('');
+    const [phone, setPhone] = React.useState('');
     const [password, setPassword] = React.useState('');
     const [firstNameError, setFirstNameError] = React.useState('');
     const [lastNameError, setLastNameError] = React.useState('');
     const [emailError, setEmailError] = React.useState('');
+    const [phoneError, setPhoneError] = React.useState('');
     const [passwordError, setPasswordError] = React.useState('');
     const [isObscure, setIsObscure] = React.useState(false);
     const [value, setValue] = React.useState('');
     const [radioError, setRadioError] = React.useState(false);
     const [helperText, setHelperText] = React.useState('');
+    const [isLoading, setIsLoading] = React.useState(false);
     
     
     
@@ -37,39 +41,73 @@ export default function Register() {
     const handleChangeEmail = (event) => {
         setEmail(event.target.value);
     };
+    const handleChangePhone = (event) => {
+        setPhone(event.target.value);
+    };
     
     const handleChangePassword = (event) => {
         setPassword(event.target.value);
     };
-    const handleSaveData = (event) => {
+
+    const validateFields = () => {
         const emailRegEx = RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
         if (email === '') {
             setEmailError('Email can\'t be empty');
+            return false;
         } else if (!emailRegEx.test(email)) {
             setEmailError('Invalid Email');
+            return false;
         } else {
+            setEmailError('');
+        }
+        if(phone === ''){
+            setPhoneError('Phone can\'t be empty');
+            return false;
+        }
+        else{
             setEmailError('');
         }
         if(firstName === ''){
             setFirstNameError('First name can\'t be empty');
+            return false;
         } else{
             setFirstNameError('');
         }
         if(lastName === ''){
             setLastNameError('Last name can\'t be empty');
+            return false;
         } else{
             setLastNameError('');
         }
         if (password === '') {
             setPasswordError('Password can\'t be empty');
+            return false;
         } else {
             setPasswordError('');
         }
         if(value === ''){
             setRadioError(true);
             setHelperText("Choose an option");
+            return false;
         }
+
+        return true;
         
+    }
+    const handleSaveData = async () => {
+        if(validateFields()){
+            setIsLoading(true);
+            const response = await registerAPI(firstName, lastName, email,phone,value, password);
+            if(response.data.result !== null){
+                setIsLoading(false);
+                if(response.data.result === false){
+                    console.log(response.data.reason);
+                } else {
+                    console.log("Register is successful and my user id is"+response.data.uid);
+                    // Router.replace('/');
+                }
+            }
+        }
     };
     
     return (
@@ -114,6 +152,15 @@ export default function Register() {
         style={{
             marginTop: "20px",
         }} onChange={handleChangeEmail} type="email" />
+        
+        <TextField id="outlined-basic" fullWidth label="Enter Phone" 
+        variant="outlined" 
+        error={phoneError === '' ? false : true}
+        helperText={phoneError}  
+        style={{
+            marginTop: "20px",
+        }} onChange={handleChangePhone} type="number" />
+        
         <div style={{
             display: 'flex',
             justifyContent: "start",
@@ -126,9 +173,9 @@ export default function Register() {
         }}>Gender</Typography>
         <FormControl error={radioError}>
         <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleRadioChange} error = {radioError === false ? false : true}row >
-        <FormControlLabel value="female" control={<Radio />} label="Female" />
-        <FormControlLabel value="male" control={<Radio />} label="Male" />
-        <FormControlLabel value="other" control={<Radio />} label="Other" />
+        <FormControlLabel value="1" control={<Radio />} label="Female" />
+        <FormControlLabel value="2" control={<Radio />} label="Male" />
+        <FormControlLabel value="3" control={<Radio />} label="Other" />
         </RadioGroup>
         <FormHelperText>{helperText}</FormHelperText>
         </FormControl>
